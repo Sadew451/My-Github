722ae16bd843316868438a17ac43da454878c32f
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-29 19:29:33 +0530

    Update antiflood_sql.py

--- NK/modules/sql/antiflood_sql.py
+++ NK/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from mrjoker.modules.sql import SESSION, BASE
+from NK.modules.sql import SESSION, BASE
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
