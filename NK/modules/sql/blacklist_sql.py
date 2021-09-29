2e8fade87e12f24a0b52280084edbd8188796afa
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-29 19:30:07 +0530

    Update blacklist_sql.py

--- NK/modules/sql/blacklist_sql.py
+++ NK/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from mrjoker.modules.sql import SESSION, BASE
+from NK.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
