e3d277e8c38702d68028cb53ffe293b7a02034ab
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-30 15:21:41 +0530

    Update modules.py

--- NK/modules/modules.py
+++ NK/modules/modules.py
@@ -28,7 +28,7 @@ def load(update: Update, context: CallbackContext):
     )
 
     try:
-        imported_module = importlib.import_module("mrjoker.modules." + text)
+        imported_module = importlib.import_module("NK.modules." + text)
     except:
         load_messasge.edit_text("Does that module even exist?")
         return
@@ -98,7 +98,7 @@ def unload(update: Update, context: CallbackContext):
     )
 
     try:
-        imported_module = importlib.import_module("mrjoker.modules." + text)
+        imported_module = importlib.import_module("NK.modules." + text)
     except:
         unload_messasge.edit_text("Does that module even exist?")
         return
