3068cf1085d503d458568bb2d3faf0c2f482eb7c
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 07:25:39 +0530

    Update urlblacklist_sql.py

--- NK/modules/sql/urlblacklist_sql.py
+++ NK/modules/sql/urlblacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from mrjoker.modules.sql import BASE, SESSION
+from NK.modules.sql import BASE, SESSION
 
 
 class URLBlackListFilters(BASE):
