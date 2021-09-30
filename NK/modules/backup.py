7cf97acacadae5eef94ecf1e9e6243a90438428b
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 07:45:38 +0530

    NK

--- NK/modules/backup.py
+++ NK/modules/backup.py
@@ -372,7 +372,7 @@ __mod_name__ = "Backup"
 __help__ = """
 *Only for group owner:*
 
-(=) /import`: Reply to the backup file for the butler / mrjoker group to import as much as possible, making transfers very easy! \ Note that files / photos cannot be imported due to telegram restrictions.
+(=) /import`: Reply to the backup file for the butler / NATSUKI group to import as much as possible, making transfers very easy! \ Note that files / photos cannot be imported due to telegram restrictions.
 (=) /export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
