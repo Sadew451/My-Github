a76b94953ba66ac41b03490524427c3c48e40864
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 07:27:44 +0530

    Update welcome_sql.py

--- NK/modules/sql/welcome_sql.py
+++ NK/modules/sql/welcome_sql.py
@@ -2,8 +2,8 @@ import random
 import threading
 from typing import Union
 
-from mrjoker.modules.helper_funcs.msg_types import Types
-from mrjoker.modules.sql import BASE, SESSION
+from NK.modules.helper_funcs.msg_types import Types
+from NK.modules.sql import BASE, SESSION
 from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
