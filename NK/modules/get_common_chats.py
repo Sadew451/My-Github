62373fa63c3d59cc579e2845d91041ec7594b3e2
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 10:35:23 +0530

    NK Updated

--- NK/modules/get_common_chats.py
+++ NK/modules/get_common_chats.py
@@ -6,9 +6,9 @@ from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 
-from mrjoker import OWNER_ID, dispatcher
-from mrjoker.modules.helper_funcs.extraction import extract_user
-from mrjoker.modules.sql.users_sql import get_user_com_chats
+from NK import OWNER_ID, dispatcher
+from NK.modules.helper_funcs.extraction import extract_user
+from NK.modules.sql.users_sql import get_user_com_chats
 
 
 @run_async
