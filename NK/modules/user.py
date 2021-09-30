8cfd51f05ecf1095a4119ce8f1c86341230a5676
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 13:20:18 +0530

    NK Update

--- NK/modules/user.py
+++ NK/modules/user.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import mrjoker.modules.sql.users_sql as sql
-from mrjoker import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from mrjoker.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from mrjoker.modules.sql.users_sql import get_all_users
+import NK.modules.sql.users_sql as sql
+from NK import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from NK.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from NK.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
