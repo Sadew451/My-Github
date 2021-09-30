971e3af42d620ca4467af4b6409f6a6cc74ba7e4
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 13:31:55 +0530

    NK

--- NK/modules/wiki.py
+++ NK/modules/wiki.py
@@ -3,8 +3,8 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
 
-from mrjoker import dispatcher
-from mrjoker.modules.disable import DisableAbleCommandHandler
+from NK import dispatcher
+from NK.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
