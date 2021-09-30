1b73105a7a917e97e129856311a709e337d0a2ae
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 12:14:45 +0530

    NK

--- NK/modules/modules.py
+++ NK/modules/modules.py
@@ -1,8 +1,8 @@
 import importlib
 import collections
 
-from mrjoker import dispatcher, telethn
-from mrjoker.__main__ import (
+from NK import dispatcher, telethn
+from NK.__main__ import (
     CHAT_SETTINGS,
     DATA_EXPORT,
     DATA_IMPORT,
@@ -13,7 +13,7 @@ from mrjoker.__main__ import (
     USER_INFO,
     USER_SETTINGS,
 )
-from mrjoker.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from NK.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
