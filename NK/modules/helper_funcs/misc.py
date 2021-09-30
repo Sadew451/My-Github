fd6eb97b3021371c8b65285cf0881690e67f48d5
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 14:30:15 +0530

    Update misc.py

--- NK/modules/helper_funcs/misc.py
+++ NK/modules/helper_funcs/misc.py
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("Back", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton(" Back ", callback_data="NK_back"),
-             EqInlineKeyboardButton("Next", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("««««", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("🍀 Back 🍀", callback_data="mrjoker_back"),
+             EqInlineKeyboardButton("»»»»", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="NK_back")]]
+        pairs += [[EqInlineKeyboardButton("⌫ Back", callback_data="mrjoker_back")]]
 
     return pairs
 
