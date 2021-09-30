86400f3d9abc179c6c86377f88497b4bd65ba221
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 13:35:02 +0530

    NK Updated

--- NK/modules/zombie.py
+++ NK/modules/zombie.py
@@ -5,7 +5,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChannelParticipantsAdmins, ChatBannedRights
 
-from mrjoker import DEMONS, DEV_USERS, DRAGONS, OWNER_ID, telethn
+from NK import DEMONS, DEV_USERS, DRAGONS, OWNER_ID, telethn
 
 # =================== CONSTANT ===================
 
