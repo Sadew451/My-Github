60b07f89cb87382fb71b8dec0cedcb386c8e8917
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 10:13:38 +0530

    Update dev.py

--- NK/modules/dev.py
+++ NK/modules/dev.py
@@ -6,8 +6,8 @@ from time import sleep
 from telegram import TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from mrjoker import dispatcher
-from mrjoker.modules.helper_funcs.chat_status import dev_plus
+from NK import dispatcher
+from NK.modules.helper_funcs.chat_status import dev_plus
 
 
 @run_async
