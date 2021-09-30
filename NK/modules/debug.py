e15eb74c8a31074bd1f95d462266899802a38f6b
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 10:13:13 +0530

    Update debug.py

--- NK/modules/debug.py
+++ NK/modules/debug.py
@@ -5,8 +5,8 @@ from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telethon import events
 
-from mrjoker import dispatcher, telethn
-from mrjoker.modules.helper_funcs.chat_status import dev_plus
+from NK import dispatcher, telethn
+from NK.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
