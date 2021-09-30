a44a8b81a06aaa50e7519a16a08dd1d8135ea4c5
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-30 07:22:15 +0530

    Update locks_sql.py

--- NK/modules/sql/locks_sql.py
+++ NK/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from mrjoker.modules.sql import SESSION, BASE
+from NK.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
